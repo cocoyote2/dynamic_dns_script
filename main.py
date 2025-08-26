80910477895e3f5604c2f413f89ef827ad989c04
Dimitri <maraindimitri@gmail.com>
2025-08-27 01:22:43 +0200

    Added everything but update not working

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import os
 import ovh
+import requests
 import json
 from dotenv import load_dotenv
+
+def get_old_ip():
+    dns_zone_id = client.get(f"/domain/zone/{DNS_ZONE_NAME}/record", subDomain=SUBDOMAIN_NAME,  fieldType="A")[0]
+    dns_zone = client.get(f"/domain/zone/{DNS_ZONE_NAME}/record/{dns_zone_id}")
+
+    old_ip = dns_zone.get('target')
+    
+    return old_ip, dns_zone_id
+
+def get_current_ip():
+    try:
+        return json.loads(requests.get("https://api.ipify.org?format=json", timeout=5).text.strip()).get('ip')
+    except Exception as e:
+        print(f'Unable to get current IP : {e}')
+        return None
 
 load_dotenv()
 
@@ -15,8 +31,14 @@ client = ovh.Client(
     consumer_key=os.getenv("CONSUMER_KEY"),
 )
 
-dns_zone_id = client.get(f"/domain/zone/{DNS_ZONE_NAME}/record", subDomain=SUBDOMAIN_NAME,  fieldType="A")[0]
+old_ip, dns_zone_id = get_old_ip()
+current_ip = get_current_ip()
 
-result = client.get(f"/domain/zone/{DNS_ZONE_NAME}/record/{dns_zone_id}")
+old_ip = "109.138.203.61"
 
-print(json.dumps(result, indent=4))
+if old_ip != current_ip:
+    client.put(f'/domain/zone/{DNS_ZONE_NAME}/record/{dns_zone_id}', subDomain=SUBDOMAIN_NAME, target=current_ip, ttl=60)
+
+    client.post(f"/domain/zone/{DNS_ZONE_NAME}/refresh")
+
+    print('The zone has been modified !')
